e1c9f12f4ce12a520ce671a47f152f03b09fa726
Ivan-AID <ivanadug12@gmail.com>
2025-08-23 22:08:49 +0000

    Fix function name typo and improve error handling in weather fetching

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 const API_KEY = "b56720562f50c886e3fba57595b2e87e";
 
-export async function fetchweather(city) {
+export async function fetchWeather(city) {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/3.0/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}&units=metric`);
         if (!response.ok) {
--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
+import { fetchWeather } from "./api.js";
+import { displayWeather } from "./ui.js";
+import { displayError } from "./ui.js";
+
 const weatherForm = document.querySelector("#weatherForm");
 const cityInput = document.querySelector("#cityInput");
 
 weatherForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const city = cityInput.value.trim();
-    try {
-        
-    } catch (error) {
-        
+  e.preventDefault();
+  const city = cityInput.value.trim();
+  try {
+    if (!city) {
+      throw new Error("Please enter a city name.");
     }
+    const data = await fetchWeather(city);
+    displayWeather(data);
+  } catch (error) {
+    console.error(error);
+    displayError("Sorry...An error occured while fetching data!!");
+  }
 });
